e134a9d9459985400b10a519e3ff0fb9b4d0597a
yuvank3 <yuvanksoni3@gmail.com>
2026-09-13 00:09:53 +0530

    Added Daily -temperature.cpp

---
+++ Stacks/Daily -temperature.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n,0);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && (temperatures[i] > temperatures[st.top()])){
+                int ind = st.top();
+                ans[ind] = i-ind;
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
